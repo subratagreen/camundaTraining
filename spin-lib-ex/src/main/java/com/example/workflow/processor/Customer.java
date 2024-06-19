6f19d2b3ad1b589c7734ca51e61c55e63c20af8d
Subrat Das <subratagreen@gmail.com>
2024-06-19 17:50:47 +0530

    spin library usage

---
+++ spin-lib-ex/src/main/java/com/example/workflow/processor/Customer.java
@@ -0,0 +1,24 @@
+package com.example.workflow.processor;
+
+public class Customer {
+
+	String name;
+
+	public Customer() {
+
+	}
+
+	public Customer(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ spin-lib-ex/src/main/java/com/example/workflow/processor/XmlProcessor.java
@@ -0,0 +1,57 @@
+package com.example.workflow.processor;
+
+import static org.camunda.spin.Spin.S;
+
+import static org.camunda.spin.Spin.XML;
+import static org.camunda.spin.Spin.JSON;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.spin.xml.SpinXmlElement;
+import org.springframework.stereotype.Component;
+
+@Component("xmlProcessor")
+public class XmlProcessor implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+			String xmlData = (String) execution.getVariable("xmlData");
+			SpinXmlElement xml = S(xmlData);
+			System.out.println(xml);
+			
+			Order order = XML(xmlData).mapTo(Order.class);
+			String jsonData = JSON(order).toString();
+			execution.setVariable("jsonData", jsonData);
+			
+			System.out.println(jsonData);
+			
+//			SpinXmlElement xml2 = XML(xmlData);
+//			
+//			xml2.namespace();
+//			
+//			SpinXmlElement oldElement = XML("<orderId/>");
+//			SpinXmlElement newElement = XML("<Id/>");
+//			
+//			SpinXmlElement replacedXmlData = xml2.replaceChild(oldElement, newElement);
+//			System.out.println(replacedXmlData);
+//			
+			
+			// <order> 	<orderId>ord-001</orderId> 	<orderName>mobile</orderName> </order>
+			
+			
+			
+			/*
+			 * import static org.camunda.spin.Spin.XML;
+
+SpinXmlTreeElement root = XML("<root><date/><order/></root>");
+SpinXmlTreeElement child1 = XML("<child/>");
+root.replaceChild(root.childElement("date"), child1);
+SpinXmlTreeElement child2 = XML("<child/>");
+root.childElement("order").replace(child2);
+
+
+			 */
+
+	}
+
+}
